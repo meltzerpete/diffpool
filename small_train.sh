2ebc85cf93e8d35ba78f80a97fb51660116c07c9
pete meltzer <meltzerpete@gmail.com>
2019-09-05 07:01:06 +0100

    small_train PTC

--- small_train.sh
+++ small_train.sh
@@ -3,24 +3,24 @@
 mkdir -p small_train
 
 #for DATASET in MUTAG PTC_FM NCI1 ; do
+for DATASET in PTC_FM PTC_FR PTC_MM PTC_MR ; do
 #for DATASET in NCI1 ; do
-for DATASET in PROTEINS ; do
+#for DATASET in PROTEINS ; do
     for ASSIGN in 0.1 0.2 0.3 ; do
-#    for ASSIGN in 0.3 ; do
         for H1 in 30 40 50; do
             for H2 in 30 40 50 ; do
-#                for NUM_TRAIN in 10 20 30 40 50 60 ; do
+                for NUM_TRAIN in 10 20 30 40 50 60 ; do
                     echo ${DATASET} ${ASSIGN} ${H1} ${H2}
                     python -m train \
                         --bmname=${DATASET} \
-                        --train-ratio=0.9 \
+                        --num-train=${NUM_TRAIN} \
                         --assign-ratio=${ASSIGN} \
                         --hidden-dim=${H1} \
                         --output-dim=${H2} \
                         --cuda=0 \
                         --num-classes=2 \
                         --method=soft-assign > small_train/${DATASET}-${ASSIGN}-${H1}-${H2}.log
-#                done
+                done
             done
         done
     done
